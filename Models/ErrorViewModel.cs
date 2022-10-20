cb3d2ecd660093f288978fc96c2136cd9cf5f9d7
pyLuna <114895753+pyLuna@users.noreply.github.com>
2022-10-20 13:21:43 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Total_Fire_Safety.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
